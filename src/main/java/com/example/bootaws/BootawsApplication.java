b4981c09fe30c55ae226c938cd9c8fb503e8110f
parksohee <1592698@naver.com>
2023-04-24 21:02:59 +0900

    Initial commit

---
+++ src/main/java/com/example/bootaws/BootawsApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootawsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootawsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bootaws/BootawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootawsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
